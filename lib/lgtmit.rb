16bdf5ae5479095dcc41e604ece3bde551613930
Kihaya Sugiura <kihayas@gmail.com>
2019-10-17 21:18:39 +0900

    feat: LGTM composition

--- lib/lgtmit.rb
+++ lib/lgtmit.rb
@@ -1,6 +1,31 @@
 require "lgtmit/version"
+require "mini_magick"
 
 module Lgtmit
-  class Error < StandardError; end
-  # Your code goes here...
+  POS = "0, 0"
+  TEXT = "LGTM"
+  COLOR = "#ffffff"
+  GRAV = "center"
+  FMT = "png"
+  P_SCALE = 3
+
+  def self.run(file_name)
+    image = MiniMagick::Image.open(file_name)
+
+    p_size = if image[:width] > image[:height]
+               image[:width] / P_SCALE
+             else
+               image[:height] / P_SCALE
+             end
+
+    image.combine_options do |config|
+      config.fill COLOR
+      config.gravity GRAV
+      config.pointsize p_size
+      config.draw "text #{POS} '#{TEXT}'"
+    end
+
+    image.format FMT
+    image.write "#{TEXT}_#{file_name}"
+  end
 end
