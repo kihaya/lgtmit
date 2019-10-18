1cb90106c7a5ff6127b9de6ed0de2760fea2cedd
Kihaya Sugiura <kihayas@gmail.com>
2019-10-18 14:54:27 +0900

    refactor: Slim up main logic

--- lib/lgtmit.rb
+++ lib/lgtmit.rb
@@ -4,22 +4,6 @@ require "mini_magick"
 
 module Lgtmit
   def self.run(file_name)
-    image = MiniMagick::Image.open(file_name)
-
-    p_size = if image[:width] > image[:height]
-               image[:width] / P_SCALE
-             else
-               image[:height] / P_SCALE
-             end
-
-    image.combine_options do |config|
-      config.fill COLOR
-      config.gravity GRAV
-      config.pointsize p_size
-      config.draw "text #{POS} '#{TEXT}'"
-    end
-
-    image.format FMT
-    image.write "#{TEXT}_#{file_name}"
+    Lgtmit::Compositer.new(file_name).run
   end
 end
